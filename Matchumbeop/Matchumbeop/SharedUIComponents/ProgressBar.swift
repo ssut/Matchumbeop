776e3c83bbded0f40613bc060ff4d03befaa4a94
Suhun Han <ssut@ssut.me>
2024-08-26 23:36:48 +0900

    feat(progress-bar): for error

--- Matchumbeop/Matchumbeop/SharedUIComponents/ProgressBar.swift
+++ Matchumbeop/Matchumbeop/SharedUIComponents/ProgressBar.swift
@@ -2,15 +2,17 @@ import SwiftUI
 
 struct ProgressBar: View {
     @Binding var progress: Double
+    @Binding var isError: Bool
     @State private var isVisible = true
     
     var body: some View {
         ZStack(alignment: .leading) {
             GeometryReader { geometry in
                 Rectangle()
-                    .fill(Color.blue)
+                    .fill(isError ? Color.red.opacity(0.75) : Color.blue.opacity(0.8))
                     .frame(width: CGFloat(self.progress / 100) * geometry.size.width)
                     .animation(.easeOut(duration: 0.2), value: progress)
+                    .animation(.easeInOut(duration: 0.2), value: isError)
             }
             .frame(height: 4)
             .background(.clear)
